b76e17b345c3bc05d3ff5b6ae2dd5aa5407e5b71
Rajkishor Thakur <rajkishort596@gmail.com>
2025-03-24 09:33:07 +0530

    Learned ejs and view Directory

---
+++ Node js/ejs/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+
+app.set("view engine", "ejs"); // set the view engine to ejs
+app.set("views", path.join(__dirname, "/views")); // set the views directory
+
+app.get("/", (req, res) => {
+  res.render("home.ejs"); // render the home.ejs file
+});
+app.get("/about", (req, res) => {
+  res.send("welcome to about section");
+});
